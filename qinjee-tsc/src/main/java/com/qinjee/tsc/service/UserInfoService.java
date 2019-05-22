efc520a625783b7ae6b9649c6ac73abe9520e996
admin <zhouyun@qinjee.cn>
2019-05-22 13:51:05 +0800

    配置log4j，以及添加alibaba的json库

--- qinjee-tsc/src/main/java/com/qinjee/tsc/service/UserInfoService.java
+++ qinjee-tsc/src/main/java/com/qinjee/tsc/service/UserInfoService.java
@@ -1,26 +1,34 @@
 package com.qinjee.tsc.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
 import com.qinjee.tsc.dao.UserInfoDao;
 import com.qinjee.tsc.model.UserInfoModel;
 
 @Service("userInfoService")
 public class UserInfoService {
 
+	private static Logger logger = LoggerFactory.getLogger(UserInfoService.class);
+	
 	@Autowired
 	private UserInfoDao userInfoDao;
 	
 	public UserInfoModel selectByPrimaryKey(Integer id) {
+		logger.info("请求参数：id={}", id);
 		return userInfoDao.selectUserInfoById(id);
 	}
 	
 	public UserInfoModel selectByUsernameAndPassword(UserInfoModel userInfo) {
+		logger.info("请求参数：userInfo={}", JSON.toJSONString(userInfo));
 		return userInfoDao.selectByUsernameAndPassword(userInfo);
 	}
 	
 	public UserInfoModel addUserInfo(UserInfoModel userInfo) {
+		logger.info("请求参数：userInfo={}", JSON.toJSONString(userInfo));
 		userInfoDao.addUserInfo(userInfo);
 		return userInfo;
 	}
