466cc5c9498efbfe16493e048fde8f743a25c3eb
admin <zhouyun@qinjee.cn>
2019-05-26 01:02:12 +0800

    tsc优化登录、注销

--- qinjee-tsc/src/main/java/com/qinjee/tsc/controller/UserInfoController.java
+++ qinjee-tsc/src/main/java/com/qinjee/tsc/controller/UserInfoController.java
@@ -1,6 +1,8 @@
 package com.qinjee.tsc.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qinjee.tsc.rabbitmq.SendMsgService;
+import com.qinjee.tsc.redis.RedisService;
 import com.qinejee.consts.ResponseConsts;
 import com.qinjee.entity.ResultJsonEntity;
 import com.qinjee.tsc.model.UserInfoModel;
@@ -36,6 +39,9 @@ public class UserInfoController {
 	
 	@Autowired
 	private SendMsgService sendMsgService;
+	
+	@Autowired
+	private RedisService redisService;
 
 	/**
 	 * 功能描述：设置redis缓存，并返回value
@@ -52,7 +58,7 @@ public class UserInfoController {
 	 * @update:[变更日期YYYY-MM-DD][更改人姓名][变更描述]
 	 */
 	@RequestMapping("/login")
-	public ResultJsonEntity login(HttpServletRequest request, String username, String password) {
+	public ResultJsonEntity login(HttpServletRequest request, HttpServletResponse response, String username, String password) {
 		
         logger.info("Login request parameter：username={};password={}", username,password);
         
@@ -63,11 +69,18 @@ public class UserInfoController {
 			userInfo.setPassword(password);
 			userInfo = userInfoService.selectByUsernameAndPassword(userInfo);
 			if (userInfo != null) {
-				logger.info("Login success！username={}", username);
+				String loginKey = ResponseConsts.SESSION_KEY + username;
+				// 设置redis登录缓存时间，30分钟过期，与前端保持一致
+				redisService.setex(loginKey, ResponseConsts.SESSION_INVALID_SECCOND, username);
+				Cookie cookie = new Cookie(ResponseConsts.SESSION_KEY, loginKey);
+				cookie.setMaxAge(ResponseConsts.SESSION_INVALID_SECCOND); 
+				cookie.setPath("/");
+				response.addCookie(cookie);
 				
 				resultJson.setResultCode(ResponseConsts.RESULT_CODE_SUCCESS);
 				resultJson.setResultStatus(ResponseConsts.RESULT_STATUS_SUCCESS);
 				resultJson.setResult(userInfo);
+				logger.info("Login success！username={}", username);
 			}else {
 				logger.info("Login faild,No this username found! username={};password={}", username,password);
 				
@@ -126,6 +139,49 @@ public class UserInfoController {
 			resultJson.setResultStatus(ResponseConsts.RESULT_STATUS_EXCEPTION);
 			resultJson.setResult(ResponseConsts.RESULT_RESULT_MESSAGE);
 		}
+		
+		return resultJson;
+	}
+	
+
+	/**
+	 * 功能描述：退出
+	 *
+	 * @param request
+	 * @param id
+	 * @return
+	 * 
+	 * @author 周赟
+	 *
+	 * @since 2019年5月15日
+	 *
+	 * @update:[变更日期YYYY-MM-DD][更改人姓名][变更描述]
+	 */
+	@RequestMapping("/logout")
+	public ResultJsonEntity logout(HttpServletRequest request, HttpServletResponse response) {
+		
+		Cookie cookies[] = request.getCookies();
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
+				if (ResponseConsts.SESSION_KEY.equals(cookies[i].getName())) {
+					String loginKey = cookies[i].getValue();
+					if (loginKey != null) {
+						if(redisService.exists(loginKey)) {
+							redisService.del(loginKey);
+						}
+						cookies[i].setValue(null);  
+						cookies[i].setMaxAge(0);// 立即销毁cookie  
+						cookies[i].setPath("/");  
+                        response.addCookie(cookies[i]); 
+					}
+				}
+			}
+		}
+		
+		ResultJsonEntity resultJson = new ResultJsonEntity();
+		resultJson.setResultCode(ResponseConsts.SESSION_LOGOUT_CODE);
+		resultJson.setResultStatus(ResponseConsts.SESSION_LOGOUT_STATUS);
+		resultJson.setResult("退出成功");
 		
 		return resultJson;
 	}
